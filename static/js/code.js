11d2bb9a4a3fdb532cc457a9a1ea4fedfb473368
ESFLOWNK <otakuflownk@gmail.com>
2024-12-23 15:54:43 -0400

    It works, not a miracle, style isn't complete

--- static/js/code.js
+++ static/js/code.js
@@ -4,15 +4,24 @@ function setDefaultSchedule(){
 
     for(i = 6; i < 22; i++){
         const tr = document.createElement("tr");
+        tr.onclick = modifyTask;
+        tr.id = (i-6).toString();
         const td1 = document.createElement("td");
         const td2 = document.createElement("td");
         let noon = "a. m.";
         if (i > 11) noon = "p. m.";
         td1.appendChild(document.createTextNode((i%12+1).toString()+":00 "+noon));
-        td2.appendChild(document.createTextNode("No task set"));
+        const taskIn = document.createElement("input");
+        taskIn.placeholder = "No task set";
+        taskIn.className = "taskInput";
+        td2.appendChild(taskIn);
         tr.appendChild(td1);
         tr.appendChild(td2);
 
         table.appendChild(tr);
     }
+}
+
+function modifyTask(event) {
+    console.log(event.target.parentNode.id);
 }
