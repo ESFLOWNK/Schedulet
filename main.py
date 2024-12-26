83a7bf7f0bf84ace33787c26e5e597a1449eed80
ESFLOWNK <otakuflownk@gmail.com>
2024-12-26 15:58:25 -0400

    Little Arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import asyncio
 from flask import Flask, render_template
+from webbrowser import open
 
 app = Flask(__name__)
 
@@ -6,5 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+async def server_run():
+    await asyncio.to_thread(app.run, "127.0.0.1", 5000)
+
+async def openBrowser():
+    await asyncio.sleep(2)
+    open("http://127.0.0.1:5000/",2,True)
+
+async def main():
+    await asyncio.gather(
+        server_run(),
+        openBrowser()
+    )
+
 if __name__ == "__main__":
-    app.run("127.0.0.1",5000, debug=True)
+    asyncio.run(main())
