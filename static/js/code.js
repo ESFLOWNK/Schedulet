8b811a4b5b15845754be6d546719a46e9c6aaf66
ESFLOWNK <otakuflownk@gmail.com>
2024-12-26 17:10:46 -0400

    Noon error fixed

--- static/js/code.js
+++ static/js/code.js
@@ -16,7 +16,7 @@ function setDefaultSchedule(){
         const td1 = document.createElement("td");
         const td2 = document.createElement("td");
         let noon = "a. m.";
-        if (i > 11) noon = "p. m.";
+        if (i > 12) noon = "p. m.";
         td1.appendChild(document.createTextNode((i%12+1).toString()+":00 "+noon));
         td2.appendChild(document.createTextNode("No task set"));
         tr.appendChild(td1);
